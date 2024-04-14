86d8f16cdec0e9c4f00268f066d0887ecf407ba9
unstoppableayush <ayushkum884@gmail.com>
2024-04-14 14:45:07 +0530

    footer icon changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,44 +1,45 @@
 import React from "react";
-import { AiOutlineGlobal } from "react-icons/ai";
-import { BsTwitterX } from "react-icons/bs";
-import {
-  FaFacebookSquare,
-  FaGithubSquare,
-  FaInstagram,
-} from "react-icons/fa";
 import FooterItem from "./FooterItem";
 import { footerItems } from "../constants";
-
+import { AiOutlineGlobal } from "react-icons/ai";
+import { FaGithubSquare } from "react-icons/fa";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { BsMedium } from "react-icons/bs";
+import { BiLogoGmail } from "react-icons/bi";
 
 const Footer = () => {
   return (
     <div className="w-full pt-10 border-t border-neutral-800">
       <div className=" max-w-[1240px] mx-auto py-15 px-4 grid lg:grid-cols-3 gap-8  ">
-        <div>
-        <div className="flex sm:justify-center md:justify-center lg:justify-start items-center ">
-        <span><AiOutlineGlobal className='fill-yellow-600 text-8xl me-2'/></span>
-          <h3 className="text-wrap w-40 text-3xl  font-bold ">
-            Global Investors Forum
-          </h3>
-        </div>
-          
+        <div >
+          <div className="flex md:justify-center lg:justify-start items-center  ">
+            <span>
+              <AiOutlineGlobal className="fill-yellow-600 text-8xl me-2" />
+            </span>
+            <h3 className="text-wrap w-40 text-3xl  font-bold ">
+              Global Investors Forum
+            </h3>
+          </div>
+
           <p className="py-4 lg:text-start md:text-center sm:text-center text-neutral-400">
             Connecting world through one platform.
           </p>
           <div className="flex justify-around md:w-[65%] sm:w-[50%] mt-3  hover:cursor-pointer ">
-            <FaFacebookSquare size={30} className=" hover:fill-blue-800 ..." />
-            <FaInstagram size={30} className="hover:fill-purple-700"  />
-            <BsTwitterX size={29}  className=" hover:fill-neutral-800 ..." />
-            <FaGithubSquare size={30} className=" hover:fill-blue-800 ..." />
+            <AiOutlineLinkedin size={30} className=" hover:fill-blue-800 ..." />
+            <BsMedium size={30} className="hover:fill-neutral-800" />
+            <BiLogoGmail size={30} className=" hover:fill-red-900 ..." />
+            <FaGithubSquare size={30} className=" hover:fill-blue-800 " />
           </div>
         </div>
-        <div className="lg:col-span-2 flex justify-between mt-6 ">  
-            {footerItems.map((category, index) => (
-              <FooterItem key={index} category={category} />
-            ))}
+        <div className="lg:col-span-2 flex justify-between mt-6 ">
+          {footerItems.map((category, index) => (
+            <FooterItem key={index} category={category} />
+          ))}
         </div>
       </div>
-      <p className="text-center pt-3 pb-7 font-medium mx-2 text-wrap text-neutral-400">Global Investors Forum &copy; 2024 All Rights Reserved</p>
+      <p className="text-center pt-3 pb-7 font-medium mx-2 text-wrap text-neutral-400">
+        Global Investors Forum &copy; 2024 All Rights Reserved
+      </p>
     </div>
   );
 };
