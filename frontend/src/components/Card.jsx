717757235f36094347747a8cbfd5447118208412
unstoppableayush <ayushkum884@gmail.com>
2024-04-14 13:28:39 +0530

    added margin top in card page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = () => {
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
         Domains
       </h2>
-      <div className=" m-10  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  ">
+      <div className="m-10 mt-20  grid sm:grid-cols-1 sm:m-10 md:grid-cols-2 lg:grid-cols-3 gap-4  ">
         {cards.map((domain, index) => (
           <div key={index} className="w-full  ">
             <ImageCard
