f9cd47cc0bf6164541600efb1874bf8662dfdfdb
unstoppableayush <ayushkum884@gmail.com>
2024-04-23 19:02:26 +0530

    changes

--- frontend/src/components/GAIS/Card.jsx
+++ frontend/src/components/GAIS/Card.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Card = ({imgUrl,desc,head}) => {
   return (
     <div className="mt-5 flex h-full w-full ">
-        <div className='md:text-2xl text-xl text-neutral-50 font-bold w-1/3'>{head}</div>
-        <div className='md:text-lg text-base text-neutral-300 ps-2 w-2/3'>{desc}</div>
+        <div className='md:text-2xl text-md text-neutral-50 font-bold w-1/3 me-2 '>{head}</div>
+        <div className='md:text-lg text-base text-neutral-300 w-2/3'>{desc}</div>
     </div>
   
   )
