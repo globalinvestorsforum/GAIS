4dff766ed5b7713b683f1e00dad04af0a25e8e60
unstoppableayush <ayushkum884@gmail.com>
2024-04-23 19:03:50 +0530

    changes

--- frontend/src/components/Ticket/Ticket.jsx
+++ frontend/src/components/Ticket/Ticket.jsx
@@ -4,8 +4,8 @@ import MyForm from './MyForm'
 const Ticket = () => {
   return (
     <div className="bg-gradient-to-r text-center from-orange-500 to-purple-600 h-full w-full min-h-[500px]">
-       <div className='text-5xl pt-10 font-bold'>Get Notified When Registration is Open!</div>
-       <div className='text-xl pt-10'>Enter your details below to be notified once registration has opened.</div>
+       <div className='md:text-5xl text-2xl pt-10 font-bold'>Get Notified When Registration is Open!</div>
+       <div className='md:text-xl text-lg  pt-10'>Enter your details below to be notified once registration has opened.</div>
         <div className='flex justify-center items-center text-start'>
           <MyForm/>
         </div>
