a8ab65554e01cfcffa3950f85d6b78c176ef8989
unstoppableayush <ayushkum884@gmail.com>
2024-05-03 11:52:08 +0530

    added animation in numbers

--- frontend/src/components/GAIS/Gais.jsx
+++ frontend/src/components/GAIS/Gais.jsx
@@ -5,8 +5,13 @@ import Banner from "./Banner";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import MyForm from "../Ticket/MyForm";
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
+import { useState } from "react";
 
 const Gais = () => {
+  const [counterOn , setcounterOn] = useState(false);
+
   return (
     <>
       <section>
@@ -40,34 +45,36 @@ const Gais = () => {
         </div>
       </section>
       <section>
+        <ScrollTrigger onEnter={()=> setcounterOn(true)} onExit={()=> setcounterOn(false)}>
         <div className="min-h-[600px]  bg-white text-blue-950">
           <div className="p-5 pt-20 md:p-32 md:text-center ">
             <div className="md:flex flex-wrap  w-full ">
               <div className="md:w-1/2  font-bold md:text-7xl text-5xl">
-                5000+
+              {counterOn && <CountUp start={0} end={5000}  duration={3} />}+
                 <hr className="w-full h-[2.5px] my-1 border-[1px] border-black md:hidden" />
                 <p className="text-xl  md:text-3xl ">Expected Visitors</p>
               </div>
               <div className="md:w-1/2 pt-10 md:p-0 font-bold md:text-7xl text-5xl">
-                10,000+
+              {counterOn && <CountUp start={0} end={10000}  duration={3} />}+
                 <hr className="w-full h-[2.5px] my-1 border-[1px] border-black md:hidden" />
                 <p className="text-xl  md:text-3xl ">Networking & Meetings</p>
               </div>
             </div>
             <div className="md:flex flex-wrap  w-full md:pt-24 pt-10  ">
               <div className="md:w-1/2 font-bold md:text-7xl text-5xl">
-                100+
+              {counterOn && <CountUp start={0} end={100}  duration={3} />}+
                 <hr className="w-full h-[2.5px] my-1 border-[1px] border-black md:hidden" />
                 <p className="text-xl  md:text-3xl ">Conferencess</p>
               </div>
               <div className="md:w-1/2 md:py-0 py-10 font-bold  md:text-7xl text-5xl">
-                <p>200+</p>
+              {counterOn && <CountUp start={0} end={200}  duration={3} />}+
                 <hr className="w-full h-[2.5px] my-1 border-[1px] border-black md:hidden" />
                 <p className="text-xl  md:text-3xl ">Speakers</p>
               </div>
             </div>
           </div>
         </div>
+        </ScrollTrigger>
       </section>
       <section className="border-y border-neutral-500">
         <div className="w-full h-[520px] bg-gradient-to-r from-orange-700 to-purple-600">
