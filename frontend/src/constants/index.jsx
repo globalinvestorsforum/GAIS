8dfd69c7239efc8288fbd6dc57c739873c078e42
globalinvestorsforum <akashofficialnyk@gmail.com>
2024-05-17 14:09:26 +0530

    fixed url

--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -338,7 +338,7 @@ export const features =[
     { icon: <AiOutlineGlobal/> , text:"Connecting with a global network of pioneers." , description:"Forge meaningful connections with fellow AI enthusiasts, entrepreneurs, investors, and researchers. Collaborate, share ideas, and build the future of AI together." }
 ]
 
-const gifURL = "global-investors-forum.com"
+const gifURL = "https://www.global-investors-forum.com"
 export const footerItems = [
     {
       title: 'AI Summit',
