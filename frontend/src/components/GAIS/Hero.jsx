7fe003118f2e1564df216aa42ec1355c5b760627
Ayush Kumar <ayushkum884@gmail.com>
2024-09-03 08:50:40 +0530

    date chnaged

--- frontend/src/components/GAIS/Hero.jsx
+++ frontend/src/components/GAIS/Hero.jsx
@@ -18,7 +18,7 @@ function Hero() {
               LATEST ADVANCEMENTS AND TRENDS OF AI.
             </h2>
             <p className="pt-6 px-3  text-lg md:text-1xl lg:text-2xl font-semibold  text-neutral-200  animate-slidein [--slidein-delay:400ms]  opacity-0 ">
-              7th & 8th November, 2024
+              6th & 7th March, 2025
             </p>
             <p className="pt-4 px-3 text-lg md:text-1xl lg:text-2xl font-semibold    text-neutral-200 animate-slidein [--slidein-delay:500ms]  opacity-0  ">
               Paris, France.
